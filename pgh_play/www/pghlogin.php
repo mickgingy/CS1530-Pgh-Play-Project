75fba322acf435bf27725b5b616b6c1a333b8ecd
nar49 <nar49@pitt.edu>
2015-06-23 21:28:36 -0400

    User login
    php script for user verification, login.html a skeleton just for testing
    purposes. the init.php sets up a local SQL database with user and hashed
    passwords.

---
+++ pgh_play/www/pghlogin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<?php
+# User login PGH_Play
+session_start();
+#connect to db
+	$db = new mysqli('localhost', 'root', 'root', 'pghplay');
+      if ($db->connect_error):
+         die ("Could not connect to db: " . $db->connect_error);
+      endif;
+	  $pass = hash('sha256', rtrim($_POST["password"]));
+		$query = "SELECT * FROM users  WHERE user='$_POST[username]' && password='$pass'";
+		$result = $db->query($query) or trigger_error(mysql_error()." ".$query);
+		$pws = $result->fetch_all(MYSQLI_ASSOC);
+		if ($result->num_rows ==1)
+		{
+			$_SESSION['user'] = $_POST['username'];
+			echo "Welcome back $_SESSION[user]!";
+		}
+		else
+		{
+			header('Location: http://localhost:8666/PGHlogin.html');
+		}
+
+?>
