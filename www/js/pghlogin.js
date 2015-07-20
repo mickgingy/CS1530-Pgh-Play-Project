6ec4cfa7aebe930435be19b41690e7882d31f431
Mick Lichtenberg <mrl69@pitt.edu>
2015-07-20 15:38:39 -0400

    Login and New User pages now fully functional with CSS and JS, and seem to integrate fine with the back-end (which I had to make minor adjustments to)!

--- www/js/pghlogin.js
+++ www/js/pghlogin.js
@@ -4,21 +4,34 @@
  *	Simple function to create a cookie called "user" that expires one day after its creation.
  */
 function login() {
-	$.post("php/pghlogin.php", $('#form').serialize(),  
-		function(data) {
-			alert("yas");
-			var response = data;
-	
+	ajax = new XMLHttpRequest();
+  	ajax.onreadystatechange = function() {
+		if (ajax.readyState == 4 && ajax.status == 200) {
+			var response = ajax.responseText;
+
 			if (response == "success") {
 			    date = new Date();
 			    date.setTime(date.getTime() + (24*60*60*1000));
 				document.cookie = "user=true; expires=" + date.toGMTString();
 				window.location.href = "index.html";
 			}
-			else {
+			else if (response == "failure") {
 				window.location.href = "pghlogin.html";
 			}
-		});
+			else {
+				alert("wut");
+			}
+		}
+	}
+	
+	var email = document.getElementById("email").value;
+	var password = document.getElementById("password").value;
+	
+	ajax.open("POST", "php/pghlogin.php", true);	
+	ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	ajax.send("email=" + email + "&password=" + password);
+}
+	
 	
 	/*
 	ajax = new XMLHttpRequest();
@@ -45,4 +58,4 @@ function login() {
 	ajax.open("POST", "php/pghlogin.php", true);	
 	ajax.setRequestHeader("Content-type", "");
 	ajax.send("username=" + email);*/
-}
+//}
